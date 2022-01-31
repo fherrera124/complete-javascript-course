21f153cbfe808c8e0b25b14d79c26a29652bf6f2
Francisco Herrera <fran.herrera124@gmail.com>
2022-01-30 23:36:49 -0300

    avances en el curso de javascript

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -45,6 +45,7 @@ const age2 = calcAge1(1991);
 
 console.log(age1, age2); */
 
+/////////////////////////////////////////
 // Arrow function (an special function expression)
 /* const calcAge3 = (birthYear) => 2037 - birthYear;
 const age3 = calcAge3(1991);
@@ -59,4 +60,262 @@ const yearsUntilRetirement = (birthYear, firstName) => {
 console.log(yearsUntilRetirement(1991, 'Jonas'));
 console.log(yearsUntilRetirement(1980, 'Bob')); */
 
-// next class video: 036
+///////////////////////////////////////
+// Functions Calling Other Functions
+/* function cutFruitPieces(fruit) {
+  return fruit * 4;
+}
+
+function fruitProcessor(apples, oranges) {
+  const applePieces = cutFruitPieces(apples);
+  const orangePieces = cutFruitPieces(oranges);
+
+  const juice = `Juice with ${applePieces} pieces of apple and ${orangePieces} pieces of orange.`;
+  return juice;
+}
+
+console.log(fruitProcessor(2, 3)); */
+
+/* const calcAge = function(birthYear) {
+  return 2037 - birthYear;
+}
+
+const yearsUntilRetirement = function (birthYear, firstName) {
+  const age = calcAge(birthYear);
+  const retirement = 65 - age;
+  // return `${firstName} retires in ${retirement}`;
+
+  if(retirement > 0) {
+    return retirement;
+  } else {
+    return -1;
+  }
+};
+
+console.log(yearsUntilRetirement(1991, 'Jonas'));
+console.log(yearsUntilRetirement(1970, 'Mike'));
+console.log(yearsUntilRetirement(1950, 'Sandra')); */
+
+///////////////////////////////////////
+// Coding Challenge #1
+
+/*
+Back to the two gymnastics teams, the Dolphins and the Koalas! There is a new gymnastics discipline, which works differently.
+Each team competes 3 times, and then the average of the 3 scores is calculated (so one average score per team).
+A team ONLY wins if it has at least DOUBLE the average score of the other team. Otherwise, no team wins!
+
+1. Create an arrow function 'calcAverage' to calculate the average of 3 scores
+2. Use the function to calculate the average for both teams
+3. Create a function 'checkWinner' that takes the average score of each team as parameters ('avgDolhins' and 'avgKoalas'),
+   and then logs the winner to the console, together with the victory points, according to the rule above. Example: "Koalas
+   win (30 vs. 13)".
+4. Use the 'checkWinner' function to determine the winner for both DATA 1 and DATA 2.
+5. Ignore draws this time.
+
+TEST DATA 1: Dolphins score 44, 23 and 71. Koalas score 65, 54 and 49
+TEST DATA 2: Dolphins score 85, 54 and 41. Koalas score 23, 34 and 27
+
+HINT: To calculate average of 3 values, add them all together and divide by 3
+HINT: To check if number A is at least double number B, check for A >= 2 * B. Apply this to the team's average scores 😉
+
+GOOD LUCK 😀
+*/
+
+/* const calcAverage = (a, b, c) => (a + b + c) / 3;
+
+function checkWinner(avgDolphins, avgKoalas) {
+  if (avgDolphins < avgKoalas * 2 && avgKoalas < avgDolphins * 2) {
+    console.log("No team wins");
+  } else if (avgDolphins > avgKoalas) {
+    console.log(`Dolphins win (${avgDolphins} vs ${avgKoalas})`);
+  } else {
+    console.log(`Koalas win (${avgKoalas} vs ${avgDolphins})`);
+  }
+}
+
+
+// Test 1
+let avgDolphins = calcAverage(44, 23, 71);
+let avgKoalas = calcAverage(65, 54, 49);
+
+checkWinner(avgDolphins, avgKoalas);
+
+// Test 2
+avgDolphins = calcAverage(85, 54, 41);
+avgKoalas = calcAverage(23, 34, 27);
+
+checkWinner(avgDolphins, avgKoalas); */
+
+////////////// Arrays
+
+/* const friends = ["Michael", "Steven", "Peter"];
+console.log(friends);
+
+const y = new Array(1991, 1984, 2021, 1996);
+
+console.log(friends[0]);
+
+console.log(friends[2]);
+
+console.log(friends.length);
+console.log(friends[friends.length - 1]);
+
+friends[2] = "Jay";
+console.log(friends);
+
+// friends = ['Bob', 'Alice'];
+// friends its a const variable, cannot change the object it stores (array) for another value,
+// but this stored array can mutate
+
+const firstName = "Jonas";
+
+const jonas = [firstName, "Schmedtmann", 2037 - 1991, "teacher", friends];
+
+console.log(jonas);
+console.log(jonas.length);
+
+// Exercise
+function calcAge(birthYear) {
+  return 2037 - birthYear;
+}
+
+const years = new Array(1991, 1984, 2021, 1996, 2002);
+
+const age1 = calcAge(years[0]);
+const age2 = calcAge(years[1]);
+const age3 = calcAge(years[years.length - 1]);
+
+console.log(age1, age2, age3);
+
+const ages = [
+  calcAge(years[0]),
+  calcAge(years[1]),
+  calcAge(years[years.length - 1]),
+];
+
+console.log(ages); */
+
+/* const friends = ["Michael", "Steven", "Peter"];
+
+//Add elements
+const newLenght = friends.push('Jay');
+console.log(friends);
+console.log(newLenght);
+
+friends.unshift('John');
+console.log(friends);
+
+// Remove elements
+friends.pop(); // Last
+const poppedElement = friends.pop();
+console.log(poppedElement);
+console.log(friends);
+
+friends.shift(); // First
+console.log(friends);
+
+
+
+console.log(friends.indexOf('Steven'));
+console.log(friends.indexOf('Bob')); // returns -1
+
+friends.push(23);
+console.log(friends.includes('Steven'));
+console.log(friends.includes('Bob'));
+console.log(friends.includes('23'));
+console.log(friends.includes(23));
+
+if(friends.includes('Steven')) {
+  console.log('You have a friend called Steven');
+} */
+
+///////////////////////////////////////
+// Coding Challenge #2
+
+/*
+Steven is still building his tip calculator, using the same rules as before: Tip 15% of the bill
+if the bill value is between 50 and 300, and if the value is different, the tip is 20%.
+
+1. Write a function 'calcTip' that takes any bill value as an input and returns the corresponding
+tip, calculated based on the rules above (you can check out the code from first tip calculator
+  challenge if you need to). Use the function type you like the most. Test the function using a
+  bill value of 100.
+2. And now let's use arrays! So create an array 'bills' containing the test data below.
+3. Create an array 'tips' containing the tip value for each bill, calculated from the function
+you created before.
+4. BONUS: Create an array 'total' containing the total values, so the bill + tip.
+
+TEST DATA: 125, 555 and 44
+
+HINT: Remember that an array needs a value in each position, and that value can actually be the
+returned value of a function! So you can just call a function as array values (so don't store
+the tip values in separate variables first, but right in the new array) 😉
+
+GOOD LUCK 😀
+*/
+
+/* // 1.
+const calcTip = (bill) =>
+  bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+
+console.log(
+  `The bill was $100, the tip was $${calcTip(100)}, and the total value $${100 + calcTip(100)}`
+);
+
+// 2.
+const bills = [125, 555, 44];
+console.log(bills);
+
+// 3.
+const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
+console.log(tips);
+
+// 4.
+const total = [bills[0]+tips[0], bills[1]+tips[1], bills[2]+tips[2]];
+console.log(total); */
+
+/////////////////////////////////////
+// Objects
+
+/* const jonas = {
+  firstName: "Jonas",
+  lastName: "Schmedtmann",
+  age: 2037 - 1991,
+  job: "teacher",
+  friends: ["Michael", "Peter", "Steven"],
+};
+console.log(jonas);
+
+console.log(jonas.lastName);
+console.log(jonas["lastName"]);
+
+// inside the square brackets we can put
+// any expression (since an expression produces values)
+const nameKey = "Name";
+console.log(jonas["first" + nameKey]);
+console.log(jonas["last" + nameKey]);
+
+const interestedIn = prompt(
+  "What do you want to know about Jonas? Choose between firstName, lastName, age, job, and friends"
+);
+
+if (jonas[interestedIn]) {
+  console.log(jonas[interestedIn]);
+} else {
+  // since undefined it's a false value
+  console.log("Wrong request!");
+}
+
+// adding new properties to the object
+jonas.location = "Portugal";
+jonas["twitter"] = "@jonasschmedtman";
+console.log(jonas);
+
+// Challenge
+// "Jonas has 3 friends, and his best friend is called Michael"
+
+console.log(
+  `${jonas.firstName} has ${jonas.friends.length} friends, and his best friend is called ${jonas.friends[0]}`
+); */
+
+// Next video: 044 Object Methods
